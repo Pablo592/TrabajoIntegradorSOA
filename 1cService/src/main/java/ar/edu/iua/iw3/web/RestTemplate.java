a792c9ec0fb2fddca2e7fe7e330970a7af3161b9
Pablo592 <pgaido524@alumnos.iua.edu.ar>
2022-09-14 18:36:42 -0300

    rutas arregladas
    Signed-off-by: Pablo592 <pgaido524@alumnos.iua.edu.ar>

--- 1cService/src/main/java/ar/edu/iua/iw3/web/RestTemplate.java
+++ 1cService/src/main/java/ar/edu/iua/iw3/web/RestTemplate.java
@@ -8,36 +8,38 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RestController;
 import ar.edu.iua.iw3.modelo.Historico;
 import ar.edu.iua.iw3.modelo.UltimoHistorico;
 
 
 @RestController
+@Component
 public class RestTemplate {
 
     private String direccionConexionB;
     private Logger log = LoggerFactory.getLogger(RestTemplate.class);
 
 
-    RestTemplate(@Value("${conexion.direccionB}") String direccionB, org.springframework.web.client.RestTemplate restTemplate){
+    RestTemplate(@Value("${conexion.direccionB}") String direccionB, RestTemplateBuilder builder){
         this.direccionConexionB = direccionB;
 
-        this.restTemplate = restTemplate;
+        this.restTemplate = builder.build();
     }
 
 
 
     private final org.springframework.web.client.RestTemplate restTemplate;
 
-
+/*
     @Autowired
     public RestTemplate(RestTemplateBuilder builder) {
         this.restTemplate = builder.build();
-    }
+    }*/
     
     public UltimoHistorico getHistoricoUltimo(String identificador) {
-        String url = "http://localhost:8090/historico/last/".concat(String.valueOf(identificador));
+        String url = "http://"+direccionConexionB+"/historico/last/".concat(String.valueOf(identificador));
         UltimoHistorico forObject = restTemplate.getForObject(url,UltimoHistorico.class);
         log.info("Result" + forObject);
         System.out.println(forObject.toString());
@@ -45,14 +47,14 @@ public class RestTemplate {
     }
     
     public Historico getHistorico(Long id) {
-        String url = "http://localhost:8090/historico/".concat(String.valueOf(id));
+        String url = "http://"+direccionConexionB+"/historico/".concat(String.valueOf(id));
         Historico forObject = restTemplate.getForObject(url, Historico.class);
         log.info("Result" + forObject);
         System.out.println(forObject.toString());
         return forObject;
     }
     public List<Historico> getHistoricosList(String pageSize,String page) {
-        String url = "http://localhost:8090/historico/all-page?size=".concat(pageSize)+"&page=".concat(page);
+        String url = "http://"+direccionConexionB+"/historico/all-page?size=".concat(pageSize)+"&page=".concat(page);
         Historico[] forObject = restTemplate.getForObject(url, Historico[].class);
         log.info("Result" + Arrays.toString(forObject));
         return  Arrays.asList(forObject);
